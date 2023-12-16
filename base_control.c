e8bac3a45fb9458504d229a22d6d4a856eecf61b
phonght32 <thanhphongho1998@gmail.com>
2023-12-16 16:53:35 +0700

    add motor control

--- base_control.c
+++ base_control.c
@@ -1,6 +1,7 @@
 #include "base_control.h"
 #include "imu.h"
 #include "madgwick/imu_madgwick.h"
+#include "stepmotor.h"
 
 
 imu_func_read_bytes mpu6050_read_bytes = NULL;
@@ -9,7 +10,8 @@ imu_func_delay delay = NULL;
 
 imu_handle_t imu_handle = NULL;
 imu_madgwick_handle_t imu_madgwick_handle = NULL;
-
+stepmotor_handle_t leftmotor_handle = NULL;
+stepmotor_handle_t rightmotor_handle = NULL;
 
 err_code_t base_control_set_hw_intf(base_control_cfg_t cfg)
 {
@@ -142,6 +144,69 @@ err_code_t base_control_imu_get_quat(float *q0, float *q1, float *q2, float* q3)
 	*q1 = quat_data.q1;
 	*q2 = quat_data.q2;
 	*q3 = quat_data.q3;
+
+	return ERR_CODE_SUCCESS;
+}
+
+err_code_t base_control_motor_init(base_control_motor_cfg_t cfg)
+{
+	err_code_t err_ret;
+
+	/* Initialize left motor */
+	leftmotor_handle = stepmotor_init();
+	if (leftmotor_handle == NULL)
+    {
+        return ERR_CODE_NULL_PTR;
+    }
+
+    stepmotor_cfg_t leftmotor_cfg = {
+        .dir = 0,
+        .duty = 0,
+        .freq_hz = 0,
+        .set_pwm_duty = cfg.leftmotor_set_pwm_duty,
+        .set_pwm_freq = cfg.leftmotor_set_pwm_freq,
+        .start_pwm = cfg.leftmotor_start_pwm,
+        .stop_pwm = cfg.leftmotor_stop_pwm
+    };
+    err_ret = stepmotor_set_config(leftmotor_handle, leftmotor_cfg);
+    if (err_ret != ERR_CODE_SUCCESS)
+    {
+        return err_ret;
+    }
+
+    err_ret = stepmotor_config(leftmotor_handle);
+    if (err_ret != ERR_CODE_SUCCESS)
+    {
+        return err_ret;
+    }
+
+    /* Initialize right motor */
+    rightmotor_handle = stepmotor_init();
+	if (rightmotor_handle == NULL)
+    {
+        return ERR_CODE_NULL_PTR;
+    }
+
+    stepmotor_cfg_t rightmotor_cfg = {
+        .dir = 0,
+        .duty = 0,
+        .freq_hz = 0,
+        .set_pwm_duty = cfg.rightmotor_set_pwm_duty,
+        .set_pwm_freq = cfg.rightmotor_set_pwm_freq,
+        .start_pwm = cfg.rightmotor_start_pwm,
+        .stop_pwm = cfg.rightmotor_stop_pwm
+    };
+    err_ret = stepmotor_set_config(rightmotor_handle, rightmotor_cfg);
+    if (err_ret != ERR_CODE_SUCCESS)
+    {
+        return err_ret;
+    }
+
+    err_ret = stepmotor_config(rightmotor_handle);
+    if (err_ret != ERR_CODE_SUCCESS)
+    {
+        return err_ret;
+    }
 
 	return ERR_CODE_SUCCESS;
 }
--- base_control.h
+++ base_control.h
@@ -28,14 +28,26 @@ extern "C" {
 #endif
 
 #include "imu_intf.h"
+#include "stepmotor.h"
 
 #include "err_code.h"
 
 typedef struct {
-	imu_func_read_bytes 	mpu6050_read_bytes;
-	imu_func_write_bytes 	mpu6050_write_bytes;
-	imu_func_delay 			delay;
+	imu_func_read_bytes 		mpu6050_read_bytes;			/*!< MPU6050 read bytes */
+	imu_func_write_bytes 		mpu6050_write_bytes;		/*!< MPU6050 write bytes */
+	imu_func_delay 				delay;						/*!< IMU delay function */
 } base_control_cfg_t;
+
+typedef struct {
+	stepmotor_func_set_pwm_duty leftmotor_set_pwm_duty;		/*!< Function set PWM duty */
+	stepmotor_func_set_pwm_freq leftmotor_set_pwm_freq;		/*!< Function set PWM frequency */
+	stepmotor_func_start_pwm 	leftmotor_start_pwm;		/*!< Function start PWM */
+	stepmotor_func_stop_pwm 	leftmotor_stop_pwm;			/*!< Function stop PWM */
+	stepmotor_func_set_pwm_duty rightmotor_set_pwm_duty;	/*!< Function set PWM duty */
+	stepmotor_func_set_pwm_freq rightmotor_set_pwm_freq;	/*!< Function set PWM frequency */
+	stepmotor_func_start_pwm 	rightmotor_start_pwm;		/*!< Function start PWM */
+	stepmotor_func_stop_pwm 	rightmotor_stop_pwm;		/*!< Function stop PWM */
+} base_control_motor_cfg_t;
 
 /*
  * @brief  	Set hardware interface functions to initialize peripherals.
@@ -85,6 +97,17 @@ err_code_t base_control_imu_update_quat(void);
  *      - Others:   		Fail.
  */
 err_code_t base_control_imu_get_quat(float *q0, float *q1, float *q2, float* q3);
+
+/*
+ * @brief  	Initialize all motor.
+ *
+ * @param   None.
+ *
+ * @return
+ *      - ERR_CODE_SUCCESS:	Success.
+ *      - Others:   		Fail.
+ */
+err_code_t base_control_motor_init(base_control_motor_cfg_t cfg);
 
 #ifdef __cplusplus
 }
